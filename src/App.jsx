350af5043e496d136884562a41fc9ddda170c3d8
ValeryManoilov <manojlovv@inbox.ru>
2025-02-28 17:13:19 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HelloPage from './components/HelloPage'
+
+function App() {
+
+  return (
+    <>
+      <HelloPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HelloPage.jsx
@@ -0,0 +1,10 @@
+const HelloPage = () => {
+    return(
+        <div>
+            <p>Username</p>
+            <p>Password</p>
+        </div>
+    )
+}
+
+export default HelloPage;
