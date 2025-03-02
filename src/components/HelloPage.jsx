c34a1fd51e339f5ca1297687f3777647da573e18
ValeryManoilov <manojlovv@inbox.ru>
2025-03-02 22:28:36 +0300

    test32 commit

--- src/components/HelloPage.jsx
+++ src/components/HelloPage.jsx
@@ -133,6 +133,7 @@ const HelloPage = () => {
         })
         .then((res) => 
         {
+            console.log(res);
             navigate("/adminpanel")
         })
         .catch((err) => console.log(err));
