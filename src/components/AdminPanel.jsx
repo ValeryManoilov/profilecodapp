3ca596f300257e289d59a4fb88fc28c9331ca21f
ValeryManoilov <manojlovv@inbox.ru>
2025-03-03 15:18:35 +0300

    test33 commit

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,7 +1,27 @@
+import axios from "axios";
+import { useState } from "react";
+
 function AdminPanel()
 {
+    const [users, setUsers] = useState([]);
+    
+    useEffect(() => {
+        axios.get("https://localhost:7062/admin/getall")
+        .then((res) => {
+            console.log(res.data)
+            setUsers(res.data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return(
-        <>Вы в админ панеле</>
+        <>
+            {
+                users.map((el, id) => (
+                    <div id={id}>{el.username}</div>
+                ))
+            }
+        </>
     )
 }
 
