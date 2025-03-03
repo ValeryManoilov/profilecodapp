ac018091307578f9820b8acdf212f1b95cbb3ba3
ValeryManoilov <manojlovv@inbox.ru>
2025-03-03 20:41:02 +0300

    test38 commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ const App = observer(() => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HelloPage />} />
-        <Route path="/profile" element={<Main />} />
-        <Route path="/adminpanel" element={<AdminPanel />} />
+        {/* <Route path="/profile" element={<Main />} /> */}
+        {/* <Route path="/adminpanel" element={<AdminPanel />} /> */}
       </Routes>
     </BrowserRouter>
   )
