c069a658c2b76f388ca08419d8792bdd5aacea28
ValeryManoilov <manojlovv@inbox.ru>
2025-03-05 15:59:39 +0300

    test103 commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import ProfilePage from './components/ProfilePage'
 import { observer } from "mobx-react"
 import AdminPanel from './components/AdminPanel'
 import StadyAppPage from './components/StadyAppPage'
+import Main from './components/MainComponents/Main'
 
 const App = observer(() => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HelloPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={<Main />} />
         <Route path="/adminpanel" element={<AdminPanel />} />
         <Route path="/rejectapp" element={<StadyAppPage stady="Reject"/>}/>
         <Route path="/waitapp" element={<StadyAppPage stady="Wait"/>}/>
