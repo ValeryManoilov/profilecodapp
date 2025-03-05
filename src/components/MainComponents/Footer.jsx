a0f71fc663b168babf8a230cb6ecfcc62d5f6948
ValeryManoilov <manojlovv@inbox.ru>
2025-03-05 16:20:14 +0300

    test109 commit

--- src/components/MainComponents/Footer.jsx
+++ src/components/MainComponents/Footer.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import {ReactComponent as VkIcon} from "../../assets/Vector.svg"
-import {ReactComponent as YoutubeIcon} from "../../assets/YouTube.svg"
+import vk from "../../assets/Vector.svg"
+import yt from "../../assets/YouTube.svg"
 
 const Footerdiv = styled.div`
 background-color: #393837;
@@ -73,9 +73,9 @@ const Footer = () => {
             <P> © 2025 </P>
         </Div1>
         <Div2>
-            <Link href="https://t.me/kod_puzzle"></Link>
-            <Link href="https://vk.com/kodprog"><VkIcon/></Link>
-            <Link href="https://vk.com/away.php?to=https%3A%2F%2Fwww.youtube.com%2Fchannel%2FUCDBChulBwtvOj3_ckO8P7sw"><YoutubeIcon/> </Link>
+            <Link href="https://t.me/kod_puzzle"><Img src={tg}/></Link>
+            <Link href="https://vk.com/kodprog"><Img src={vk}/></Link>
+            <Link href="https://vk.com/away.php?to=https%3A%2F%2Fwww.youtube.com%2Fchannel%2FUCDBChulBwtvOj3_ckO8P7sw"><Img src={yt}/> </Link>
         </Div2>
     </Footerdiv>   
   );
