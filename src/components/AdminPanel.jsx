a39bbb3b3375304d1e4a0a0c7dd56485fad24ec2
ValeryManoilov <manojlovv@inbox.ru>
2025-03-06 00:40:45 +0300

    test129 commit

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -4,33 +4,91 @@ import { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 
 export const AdminPanelContainer = styled.div`
-    
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
 `
 
+export const AdminPanelContent = styled.div`
+    width: 40%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 1em;
+`
+ 
 export const AdminPanelTitle = styled.h1`
-    
+    padding: 0;
+    margin: 0;
 `
 
 export const ApplicationContainer = styled.div`
-    
+    width: 100%;
+    padding: 5%;
+    box-sizing: border-box;
+    border: 2px solid #2384E3;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    border-radius: 1em;
 `
 
-export const Application = styled.div`
-    
+export const DataContainer = styled.div`
+    display: grid;
+    grid-template-columns: 50% 50%;
+`
+
+export const DataText = styled.p`
+    font-size: 1em;
+`
+
+export const Applications = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 1em;
+    width: 90%;
+`
+
+export const ButtonsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    gap: 2em;
 `
 
 export const Button = styled.button`
+    padding: 0.5em 1em;
+    font-size: 1em;
+    border: none;
+    cursor: pointer;
+
+    transition: all 0.5s;
+    &:hover{
+        background-color: #2384E3;
+        color: #FFFFFF;
+    }
+
+    border-radius: 1em;
 `
 
 export const ApproveButton = styled(Button)`
+    background-color: #17be17;
 
+    
 `
 
 export const RejectButton = styled(Button)`
-
+    background-color: #dd3939;
 `
 
 export const Title = styled.h1`
+    margin: 0;
+    padding: 0;
 `
 
 export const ApplicationTitle = styled.h2`
@@ -41,7 +99,7 @@ function AdminPanel()
 {
     const [users, setUsers] = useState([]);
     const [flag, setFlag] = useState(true)
-    
+
     const RejectApp = useCallback((TelegramId) => 
         {
         axios.post("https://localhost:7062/admin/rejectapp", {
@@ -88,25 +146,38 @@ function AdminPanel()
 
     return(
         <AdminPanelContainer>
-            <Title>
-                Панель администратора
-            </Title>
-            <ApplicationTitle>
-                Заявки на добавление
-            </ApplicationTitle>
-            {
-                users.map((el) => (
-                    <ApplicationContainer id={el.telegramId}>
-                        {el.userName}
-                        <ApproveButton onClick={() => ApproveApp(el.telegramId)}>
-                            Добавить
-                        </ApproveButton>
-                        <RejectButton onClick={() => RejectApp(el.telegramId)}>
-                            Отклонить
-                        </RejectButton>
-                    </ApplicationContainer>
-                ))
-            }
+            <AdminPanelContent>
+                <Title>
+                    Панель администратора
+                </Title>
+                <ApplicationTitle>
+                    Заявки на добавление
+                </ApplicationTitle>
+                <Applications>
+                    {
+                        users.map((el) => (
+                            <ApplicationContainer>
+                                <DataContainer>
+                                    <DataText>Username: {el.username}</DataText>
+                                    <DataText>Email: {el.email}</DataText>
+                                    <DataText>Group: {el.group}</DataText>
+                                    <DataText>Description: {el.description}</DataText>
+                                    <DataText>About Me: {el.aboutMe}</DataText>
+                                    <DataText>Course: {el.course}</DataText>
+                                </DataContainer>
+                                <ButtonsContainer>
+                                    <ApproveButton onClick={() => ApproveApp(el.telegramId)}>
+                                        Добавить
+                                    </ApproveButton>
+                                    <RejectButton onClick={() => RejectApp(el.telegramId)}>
+                                        Отклонить
+                                    </RejectButton>
+                                </ButtonsContainer>
+                            </ApplicationContainer>
+                        ))
+                    }
+                </Applications>
+            </AdminPanelContent>
         </AdminPanelContainer>
     )
 }
