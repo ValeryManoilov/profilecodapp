5c82c8971d96c72061465901e2398ccf053df248
ValeryManoilov <manojlovv@inbox.ru>
2025-03-06 00:44:05 +0300

    test130 commit

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -8,7 +8,7 @@ export const AdminPanelContainer = styled.div`
     height: 100vh;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: start;
     gap: 1em;
 `
 
